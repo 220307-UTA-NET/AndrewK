71075079209e07eb4f65d77c8a41ee6fa4e9fc0b
aek94 <krisnawanandrew@gmail.com>
2022-03-15 13:36:29 -0700

    Update GuessingGame

--- Week-2/GuessingGame/GuessingGame/Program.cs
+++ Week-2/GuessingGame/GuessingGame/Program.cs
@@ -1,25 +1,55 @@
-// GuessingGame -- play a quick guessing game with the computer.
-Console.WriteLine("Welcome to the Guessing Game!");
+namespace GuessingGame {
+	class Program {
+		static void Main(string[] args) {
+		// GuessingGame -- play a quick guessing game with the computer.
+		Console.WriteLine("Welcome to the Guessing Game!");
 
-// While loop to ask user whether to play again or not
-while(true) {
-	Console.WriteLine("Enter the number for the menu option of your choice: ");
-	Console.WriteLine("[1] - Play the Guessing Game.");
-	Console.WriteLine("[0] - Exit.");
-	
-	int menu = int.Parse(Console.ReadLine());
+		//var rand = new Random();	// Instantiate random number generator
 
-	// Switch statements to play the game or not	
-	switch(menu) {
-		// Exit the app
-		case 0:
-			Console.WriteLine("Good game, thanks for playing!");
-			return;
-		// Play the game
-		case 1:
+		// While loop to ask user whether to play again or not
+		while(true) {
+			Console.WriteLine("Enter the number for the menu option of your choice: ");
+			Console.WriteLine("[1] - Play the Guessing Game.");
+			Console.WriteLine("[2] - Math Challenge.");
+			Console.WriteLine("[0] - Exit.");
+			
+			int menu = int.Parse(Console.ReadLine());
+
+			// Switch statements to play the game or not	
+			switch(menu) {
+				// Exit the app
+				case 0:
+					// Prompt to clear the screen
+					Console.WriteLine("Thank you for playing!");
+					Console.WriteLine("Press Enter to continue.");
+					Console.ReadLine();
+					Console.Clear();;
+					return;
+				// Play the game
+				case 1:
+					GuessingGame();
+					break;
+				case 2:
+					AdditionChallenge();
+					break;
+				// If input is not recognized	
+				default:
+					Console.WriteLine("Invalid input!");
+
+					// Prompt to clear the screen
+					Console.WriteLine("Press Enter to continue.");
+					Console.ReadLine();
+					Console.Clear();
+					
+					break;		
+				}	
+			}
+		}
+		
+		static void GuessingGame() {
 			// Instantiate random number generator
 			var rand = new Random();
-	
+
 			// Generate a random number between 0 - 10
 			int number = rand.Next(11);
 			//Console.WriteLine(number);
@@ -35,6 +65,12 @@ while(true) {
 				// If the user guessed the correct number 
 				if(input == number) {
 					Console.WriteLine("Congratulations! You've correctly guessed the secret number!");
+					
+					// Prompt to clear the screen
+					Console.WriteLine("Press Enter to continue.");
+					Console.ReadLine();
+					Console.Clear();
+
 					break;
 				}
 				// If the user guessed too high
@@ -45,12 +81,39 @@ while(true) {
 				else {
 					Console.WriteLine("You've guessed too low!");
 				}
+			}	
+		}
+
+		static void AdditionChallenge() {
+			var rand = new Random();	// Generating random values for the problem
+			int num1 = rand.Next(101);
+			int num2 = rand.Next(101);
+
+			int solution = num1 + num2;	// Finding the correct solution
+
+			Console.WriteLine("Welcome to the Addition Challenge!");
+			
+			while(true) {
+				Console.WriteLine("Your question is: ");
+				Console.WriteLine(num1 + " + " + num2 + " = ?");
+				Console.Write("Enter your answer: ");
+
+				int userGuess = int.Parse(Console.ReadLine());	// Accepting the users solution
+
+				if(solution == userGuess) {
+					Console.WriteLine("You got it!");
+					
+					// Prompt to clear the screen
+					Console.WriteLine("Press Enter to continue.");
+					Console.ReadLine();
+					Console.Clear();
+
+					break;
+				}
+				else {
+					Console.WriteLine("Not quite, you were off by: " + Math.Abs(solution - userGuess));
+				}
 			}
-			break;
-		
-		// If input is not 0 or 1
-		default:
-			Console.WriteLine("Invalid input!");
-			break;		
+		}
 	}
-}
+}	
