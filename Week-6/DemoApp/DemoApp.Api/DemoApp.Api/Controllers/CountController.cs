e4d0b0be7f1e72eb640a71732dc0778d91ba4bb0
aek94 <krisnawanandrew@gmail.com>
2022-04-13 15:02:39 -0700

    Add Week-6 folder

---
+++ Week-6/DemoApp/DemoApp.Api/DemoApp.Api/Controllers/CountController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DemoApp.Api.Controllers
+{
+    [Route("api/[controller")]
+    [ApiController]
+    public class CountController : ControllerBase
+    {
+        // Fields
+        static private int count = 0;
+        private readonly ILogger<CountController> _logger;
+
+        // Constructors
+        public CountController(ILogger<CountController> logger)
+        {
+            this._logger = logger;
+        }
+
+        // Methods
+
+        // Every tume we call the Get method, I want to return the number of times the mothod has been called
+        // GET: api/CountController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            count++;
+            _logger.LogInformation("Count.Get executed. Count: {count}", count);
+            return new string[] { count.ToString() };
+        }
+
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        
+
+
+
+
+
+    }
+
+}
